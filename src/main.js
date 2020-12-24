fab11a9ad1fd5f4c56bb860c271276e4919fbbac
Hyrum Cooper <hc346@byu.edu>
2020-12-24 18:46:08 -0500

    installed bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+
+// Install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
