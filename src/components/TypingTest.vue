6cf9f3a1b06837e9d871112a94de3ca287f82e9c
Hyrum Cooper <hc346@byu.edu>
2020-12-24 15:55:15 -0500

    error checking; mistake logging; completion checking; word/character count computed properties

--- src/components/TypingTest.vue
+++ src/components/TypingTest.vue
@@ -3,10 +3,10 @@
     <p>{{ passage }}</p>
     <textarea
       v-model='input'
-      @keypress='onKeyPress'
+      @keydown='onKeyPress'
       @input='onType'
       class='test-textarea'
-      :class='{error: !containsError}'
+      :class='{error: containsError, complete: isComplete}'
     />
   </div>
 </template>
@@ -15,24 +15,82 @@
 export default {
   name: 'TypingTest',
   data() {
-    return { // TODO: keep track of starting time
+    return {
       passage: 'This is a difficult typing passage.',
       input: '',
-      starttime: null
+      beginTime: null,
+
+      isComplete: false,
+      containsError: false,
+
+      curWordIndex: 0,
+      mistakes: {} // dict of arrays for each each index; each array contains all incorrect spellings typed by the user
     }
   },
   props: {},
   methods: {
-    onKeyPress() {
+    onKeyPress(event) {
       // TODO: prevent user from backspacing a correct word
+
+      if (this.isComplete) return event.preventDefault();
     },
-    onType() {
+    onType(event) {
+      if (!this.beginTime) this.beginTime = Date.now(); // TODO: consolidate to its own function
+
+      this.containsError = !this.isCorrect();
+
+      // mark word as an error if typed incorrectly
+      if (this.containsError) { // TODO: consolidate to its own function
+        this.recordError();
+      }
+
+      // increase word index when space is typed following a correct sequence of words
+      // TODO: record time for each word
+      if (event.data == ' ' && !this.containsError) {
+        this.curWordIndex++;
+      }
+
+      if (!this.isComplete && this.passage == this.input) {
+        this.onComplete(); // TODO: should I use an event instead?
+      }
+    },
+    recordError() {
+      let curMistakes = this.mistakes[this.curWordIndex];
+      let curWord = this.input.split(' ')[this.curWordIndex];
+      // TODO: there's a bug when error contains a space; we need to grab substr without splitting
+
+      if (curMistakes && !curMistakes.includes(curWord)) {
+          curMistakes.push(curWord);
+      } else if (!curMistakes) {
+        this.mistakes[this.curWordIndex] = [curWord]
+      }
+    },
+    isCorrect() {
+      return this.passage.startsWith(this.input);
+    },
+    onComplete() {
+      this.isComplete = true;
+      console.log('Woohoo!', this.mistakes, this.wordsComplete, this.charactersComplete);
+
       // TODO: end timer when passage is done, accumulate errors
     }
   },
   computed: {
-    containsError() {
-      return this.passage.startsWith(this.input)
+    // Returns array of words in passage (including punctuation!)
+    words() {
+      return this.passage.split(' ');
+    },
+
+    wordsComplete() {
+      return this.curWordIndex + this.isComplete;
+    },
+
+    // TODO: research whether spaces should contribute to character count
+    charactersComplete() {
+      if (this.isCorrect()) return this.input.length;
+
+      let min = this.words.slice(0, this.curWordIndex).join(' ').length;
+      return min; // TODO: this is wrong but I'll come back to it later
     }
   }
 }
@@ -46,7 +104,11 @@ export default {
 }
 
 .test-textarea.error {
-  background-color:red;
+  background-color: red;
+}
+
+.test-textarea.complete {
+  background-color: green;
 }
 
 </style>
